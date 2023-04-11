4ba38ec752e5341abad2bea402ab93913dc31263
drprojects <dr91@wanadoo.fr>
2023-04-11 17:54:45 +0200

    update readme

--- install.sh
+++ install.sh
@@ -54,7 +54,5 @@ conda activate ${PROJECT_NAME}
 # Dependencies
 conda install pip numpy -y
 conda install -c omnia eigen3 -y
-#conda install eigen -y
-#conda install -c r libiconv -y
 
 python python/setup.py build_ext
