5302bd7c1841cde494baffb43b5b8d60f0ce5faf
Romain Janvier <romain.janvier@hotmail.fr>
2023-08-29 18:03:38 +0200

    Micro optimization + const correctness

--- src/pgeof.cpp
+++ src/pgeof.cpp
@@ -1,26 +1,24 @@
-#include <iostream>
 #include <cstdio>
+#include <cmath>
+#include <iostream>
 #include <vector>
+
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Eigenvalues>
-#include <numpy/ndarrayobject.h>
-#include <limits>
-#include <map>
-#include <cmath>
 
 namespace ei = Eigen;
 
 struct pcaOutput {
-    std::vector<float> val;
-    std::vector<float> v0;
-    std::vector<float> v1;
-    std::vector<float> v2;
+    std::array<float,3> val;
+    std::array<float,3> v0;
+    std::array<float,3> v1;
+    std::array<float,3> v2;
     float eigenentropy;
 };
 typedef struct pcaOutput PCAOutput;
 typedef ei::Matrix<float, 3, 3> Matrix3f;
 typedef ei::Matrix<float, 3, 1> Vector3f;
-
+typedef ei::Matrix<float, ei::Dynamic, 3> PointCloud;
 
 PCAOutput neighborhood_pca(
     float *xyz, uint32_t *nn, const uint32_t *nn_ptr, std::size_t i_point,
@@ -28,7 +26,7 @@ PCAOutput neighborhood_pca(
 {
     // Initialize the cloud (n_neighbors + 1, 3) matrix holding the
     // points' neighbors XYZ coordinates
-    ei::MatrixXf cloud(k_nn, 3);
+    PointCloud cloud(k_nn, 3);
 
     // Recover the neighbors' XYZ coordinates using nn and xyz
     std::size_t idx_nei;
@@ -66,19 +64,19 @@ PCAOutput neighborhood_pca(
         std::begin(indices),
         std::end(indices),
         [&](int i1, int i2) { return ev[i1] > ev[i2]; } );
-    std::vector<float> val = {
+    std::array<float,3> val = {
         (std::max(ev[indices[0]],0.f)),
         (std::max(ev[indices[1]],0.f)),
         (std::max(ev[indices[2]],0.f))};
-    std::vector<float> v0 = {
+    std::array<float,3> v0 = {
         es.eigenvectors().col(indices[0])(0).real(),
         es.eigenvectors().col(indices[0])(1).real(),
         es.eigenvectors().col(indices[0])(2).real()};
-    std::vector<float> v1 = {
+    std::array<float,3>  v1 = {
         es.eigenvectors().col(indices[1])(0).real(),
         es.eigenvectors().col(indices[1])(1).real(),
         es.eigenvectors().col(indices[1])(2).real()};
-    std::vector<float> v2 = {
+    std::array<float,3>  v2 = {
         es.eigenvectors().col(indices[2])(0).real(),
         es.eigenvectors().col(indices[2])(1).real(),
         es.eigenvectors().col(indices[2])(2).real()};
@@ -108,17 +106,15 @@ PCAOutput neighborhood_pca(
 
     // Compute the eigenentropy as defined in:
     // http://lareg.ensg.eu/labos/matis/pdf/articles_revues/2015/isprs_wjhm_15.pdf
-    float epsilon = 0.001;
-    float val_sum = val[0] + val[1] + val[2] + epsilon;
-    float e0 = val[0] / val_sum;
-    float e1 = val[1] / val_sum;
-    float e2 = val[2] / val_sum;
-    float eigenentropy = - e0 * log(e0 + epsilon) - e1 * log(e1 + epsilon)
-        - e2 * log(e2 + epsilon);
+    const float epsilon = 0.001f;
+    const float val_sum = val[0] + val[1] + val[2] + epsilon;
+    const float e0 = val[0] / val_sum;
+    const float e1 = val[1] / val_sum;
+    const float e2 = val[2] / val_sum;
+    const float eigenentropy = - e0 * std::log(e0 + epsilon) - e1 * std::log(e1 + epsilon)
+        - e2 * std::log(e2 + epsilon);
 
-    PCAOutput out = {val, v0, v1, v2, eigenentropy};
-
-    return out;
+    return {val, v0, v1, v2, eigenentropy};
 }
 
 
@@ -126,31 +122,30 @@ void compute_geometric_features(
     float *xyz, uint32_t *nn, const uint32_t *nn_ptr, int n_points,
     float *features, int k_min, int k_step, int k_min_search, bool verbose)
 {
-
     // Each point can be treated in parallel
     std::size_t s_point = 0;
     #pragma omp parallel for schedule(static)
     for (std::size_t i_point = 0; i_point < n_points; i_point++)
     {
         // Recover the points' total number of neighbors
-        std::size_t k_nn = nn_ptr[i_point + 1] - nn_ptr[i_point];
+        const std::size_t k_nn = nn_ptr[i_point + 1] - nn_ptr[i_point];
 
         // If the cloud has only one point, populate the final feature
         // vector with zeros and continue
         if (k_nn < k_min or k_nn <= 0)
         {
-            features[i_point * 12 + 0]  = 0;
-            features[i_point * 12 + 1]  = 0;
-            features[i_point * 12 + 2]  = 0;
-            features[i_point * 12 + 3]  = 0;
-            features[i_point * 12 + 4]  = 0;
-            features[i_point * 12 + 5]  = 0;
-            features[i_point * 12 + 6]  = 0;
-            features[i_point * 12 + 7]  = 0;
-            features[i_point * 12 + 8]  = 0;
-            features[i_point * 12 + 9]  = 0;
-            features[i_point * 12 + 10] = 0;
-            features[i_point * 12 + 11] = 0;
+            features[i_point * 12 + 0]  = 0.f;
+            features[i_point * 12 + 1]  = 0.f;
+            features[i_point * 12 + 2]  = 0.f;
+            features[i_point * 12 + 3]  = 0.f;
+            features[i_point * 12 + 4]  = 0.f;
+            features[i_point * 12 + 5]  = 0.f;
+            features[i_point * 12 + 6]  = 0.f;
+            features[i_point * 12 + 7]  = 0.f;
+            features[i_point * 12 + 8]  = 0.f;
+            features[i_point * 12 + 9]  = 0.f;
+            features[i_point * 12 + 10] = 0.f;
+            features[i_point * 12 + 11] = 0.f;
             continue;
         }
 
@@ -172,7 +167,7 @@ void compute_geometric_features(
             // searching at k_min_search
             std::size_t k0 = std::min(std::max(k_min, k_min_search), int(k_nn));
 
-            for (std::size_t k = k0; k < k_nn + 1; k++)
+            for (std::size_t k = k0; k < k_nn + 1; ++k)
             {
                 // Only evaluate the neighborhood's PCA every 'k_step'
                 // and at the boundary values: k0 and k_nn
@@ -182,7 +177,7 @@ void compute_geometric_features(
                 }
 
                 // Actual PCA computation on the k-neighborhood
-                PCAOutput pca_k = neighborhood_pca(xyz, nn, nn_ptr, i_point, k);
+                const PCAOutput pca_k = neighborhood_pca(xyz, nn, nn_ptr, i_point, k);
 
                 // Keep track of the optimal neighborhood size with the
                 // lowest eigenentropy
@@ -196,36 +191,36 @@ void compute_geometric_features(
         }
 
         // Recover the eigenvalues and eigenvectors from the PCA
-        std::vector<float> val = pca.val;
-        std::vector<float> v0 = pca.v0;
-        std::vector<float> v1 = pca.v1;
-        std::vector<float> v2 = pca.v2;
+        const std::array<float,3> & val = pca.val;
+        const std::array<float,3> & v0 = pca.v0;
+        const std::array<float,3> & v1 = pca.v1;
+        const std::array<float,3> & v2 = pca.v2;
 
         // Compute the dimensionality features. The 1e-3 term is meant
         // to stabilize the division when the cloud's 3rd eigenvalue is
         // near 0 (points lie in 1D or 2D). Note we take the sqrt of the
         // eigenvalues since the PCA eigenvaluess are homogeneous to m²
-        float val0       = sqrtf(val[0]);
-        float val1       = sqrtf(val[1]);
-        float val2       = sqrtf(val[2]);
-        float linearity  = (val0 - val1) / (val0 + 1e-3);
-        float planarity  = (val1 - val2) / (val0 + 1e-3);
-        float scattering = val2 / (val0 + 1e-3);
-        float length     = val0;
-        float surface    = sqrtf(val0 * val1 + 1e-6);
-        float volume     = powf(val0 * val1 * val2 + 1e-9, 1 / 3.);
-        float curvature  = val2 / (val0 + val1 + val2 + 1e-3);
+        const float val0       = std::sqrt(val[0]);
+        const float val1       = std::sqrt(val[1]);
+        const float val2       = std::sqrt(val[2]);
+        const float linearity  = (val0 - val1) / (val0 + 1e-3f);
+        const float planarity  = (val1 - val2) / (val0 + 1e-3f);
+        const float scattering = val2 / (val0 + 1e-3);
+        const float length     = val0;
+        const float surface    = std::sqrt(val0 * val1 + 1e-6);
+        const float volume     = std::pow(val0 * val1 * val2 + 1e-9f, 1.f / 3.f);
+        const float curvature  = val2 / (val0 + val1 + val2 + 1e-3f);
 
         // Compute the verticality. NB we account for the edge case
         // where all features are 0
-        float verticality = 0;
+        float verticality = 0.f;
         if (val0 > 0)
         {
-            std::vector<float> unary_vector = {
-                val[0] * fabsf(v0[0]) + val[1] * fabsf(v1[0]) + val[2] * fabsf(v2[0]),
-                val[0] * fabsf(v0[1]) + val[1] * fabsf(v1[1]) + val[2] * fabsf(v2[1]),
-                val[0] * fabsf(v0[2]) + val[1] * fabsf(v1[2]) + val[2] * fabsf(v2[2])};
-            float norm = sqrt(
+            const std::array<float,3> unary_vector = {
+                val[0] * std::abs(v0[0]) + val[1] * std::abs(v1[0]) + val[2] * std::abs(v2[0]),
+                val[0] * std::abs(v0[1]) + val[1] * std::abs(v1[1]) + val[2] * std::abs(v2[1]),
+                val[0] * std::abs(v0[2]) + val[1] * std::abs(v1[2]) + val[2] * std::abs(v2[2])};
+            const float norm = std::sqrt(
                 unary_vector[0] * unary_vector[0]
                 + unary_vector[1] * unary_vector[1]
                 + unary_vector[2] * unary_vector[2]);
@@ -250,7 +245,7 @@ void compute_geometric_features(
         // NB: when in parallel s_point behavior is undefined, but gives
         // a good indication of progress
         s_point++;
-        if (s_point % 10000 == 0 && verbose)
+        if (verbose && s_point % 10000 == 0)
         {
             std::cout << s_point << "% done          \r" << std::flush;
             std::cout << ceil(s_point * 100 / n_points) << "% done          \r" << std::flush;
